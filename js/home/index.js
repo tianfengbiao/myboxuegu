4f2548b1616a4148e9896a2f607a712e669ae9a3
TianFengbiao <18208648965@163.com>
2017-05-12 16:05:55 +0800

    修改退出功能

--- js/home/index.js
+++ js/home/index.js
@@ -1,7 +1,5 @@
-define(['bootstrap','jquery','aside','header'],function(ud,$,ud1,ud2){
-  // console.log('首页执行');
+define(['aside','header'],function(){
+  console.log('首页执行');
 
-  $('#logout').on('click', function() {
-				location.href = '/html/home/login.html';
-});
+ 
 })
